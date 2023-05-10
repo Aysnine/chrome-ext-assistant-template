ced016c490b1bed72864dabaa826f5c8973d2674
CNine <cnine229@gmail.com>
2023-05-10 21:53:08 +0800

    feat: basic action, content, setting flow

--- src/content/index.js
+++ src/content/index.js
@@ -1,44 +1,68 @@
-const ROOT_ID = 'assistant-root' // ! CHANGE THIS VALUE FOR YOUR PROJECT
+const ROOT_ID = "assistant-root"; // ! CHANGE THIS VALUE FOR YOUR PROJECT
 
-function createRoot() {
+function injectRoot() {
   const rawRoot = (() => {
-    const root = document.getElementById(`#${ROOT_ID}`)
+    const root = document.getElementById(`#${ROOT_ID}`);
 
     if (!root) {
-      const container = document.createElement('div')
-      container.id = ROOT_ID
-      container.style = 'position: absolute; width: 0; height: 0; margin: 0; padding: 0;'
-      document.body.appendChild(container)
+      const container = document.createElement("div");
+      container.id = ROOT_ID;
+      container.style =
+        "position: absolute; width: 0; height: 0; margin: 0; padding: 0;";
+      document.body.appendChild(container);
 
-      return container
+      return container;
     }
 
-    return root
-  })()
+    return root;
+  })();
 
-  const shadowRoot = rawRoot instanceof ShadowRoot ? rawRoot : rawRoot.attachShadow({ mode: 'open' })
+  rawRoot.innerHTML = "";
 
-  return shadowRoot
+  const shadowRoot =
+    rawRoot instanceof ShadowRoot
+      ? rawRoot
+      : rawRoot.attachShadow({ mode: "open" });
+
+  return { rawRoot, shadowRoot };
 }
 
-const root = createRoot()
+const { rawRoot, shadowRoot: root } = injectRoot();
 
-const style = document.createElement('style')
+// Test inline style
+const style = document.createElement("style");
 style.innerHTML = `
-  .demo-text {
-    position: fixed;
-    bottom: 40px;
-    right: 20px;
-    z-index: 9999;
-    background: #fff;
-    padding: 10px;
-    border: 1px solid #000;
+.demo-text {
+  position: fixed;
+  bottom: 40px;
+  right: 20px;
+  z-index: 9999;
+  background: #fff;
+  padding: 10px;
+  border: 1px solid #000;
+}
+`;
+root.appendChild(style);
+
+// Test fixed content
+const div = document.createElement("div");
+div.classList.add("demo-text");
+div.innerHTML = "Hello from page content";
+root.appendChild(div);
+
+// Test toggle display
+rawRoot.style.display = "none"; // default display none
+function toggleDisplay() {
+  if (rawRoot.style.display === "none") {
+    rawRoot.style.display = "block";
+  } else {
+    rawRoot.style.display = "none";
   }
-`
-root.appendChild(style)
+}
 
-const div = document.createElement('div')
-div.classList.add('demo-text')
-div.innerHTML = 'Hello from page content'
-
-root.appendChild(div)
+// Listen toggle action
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === "toggle") {
+    toggleDisplay();
+  }
+});
--- src/service/index.js
+++ src/service/index.js
@@ -1,1 +1,26 @@
-console.log('hello background script here')
+// Test tab current page content permission
+// And set popup guide if not permitted
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
+  if (changeInfo.status === "loading") {
+    try {
+      await chrome.scripting.executeScript({
+        target: { tabId },
+        function: () => {},
+      });
+
+      await chrome.action.setPopup({ tabId, popup: "" });
+    } catch (error) {
+      await chrome.action.setPopup({ tabId, popup: "src/popup/index.html" });
+    }
+  }
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: () => {},
+  });
+
+  // send message to content script
+  chrome.tabs.sendMessage(tab.id, { action: "toggle" });
+});
